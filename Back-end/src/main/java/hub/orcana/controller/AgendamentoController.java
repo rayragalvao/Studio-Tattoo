1a16d5c44f1898cf2172de7298ab35247e28721c
nicollasbispo <nicollas.bpereira@sptech.school>
2025-09-07 18:06:00 -0300

    Subindo Crud de Agendamento- validar

--- Back-end/src/main/java/hub/orcana/controller/AgendamentoController.java
+++ Back-end/src/main/java/hub/orcana/controller/AgendamentoController.java
@@ -2,9 +2,12 @@ package hub.orcana.controller;
 
 import hub.orcana.service.AgendamentoService;
 import hub.orcana.tables.Agendamento;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/agendamento")
@@ -14,4 +17,102 @@ public class AgendamentoController {
     public AgendamentoController(AgendamentoService service) {
         this.service = service;
     }
+
+    @GetMapping
+    public ResponseEntity<?> getAgendamento(){
+        try{
+            List<Agendamento> agenda = service.getAgendamentos();
+            if(agenda.isEmpty()){
+                return ResponseEntity.status(204).body(null);
+            }else{
+                return ResponseEntity.status(200).body(agenda);
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(400).body("Erro ao listar agendas" + e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAgendamentoPorId(@PathVariable Long id) {
+        try {
+            Agendamento agendamento = service.getAgendamentoPorId(id);
+            return ResponseEntity.status(200).body(agendamento);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/statusAtual/{status}")
+    public ResponseEntity<?> getAgendamentosByStatus(@PathVariable String status) {
+        try {
+            List<Agendamento> sitStatus = service.getAgendamentosByStatus(status);
+            if (sitStatus.isEmpty()) {
+                return ResponseEntity.status(204).body(null);
+            }
+            return ResponseEntity.ok(sitStatus); // Retorna a lista completa
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Erro ao buscar situação do status: " + e.getMessage());
+        }
+    }
+
+ /*   @GetMapping("/dataHora/{dthora}")
+    public ResponseEntity<?> getAgendamentosByDataHora(@PathVariable String dthora) {
+        try {
+            List<Agendamento> diaHora = service.getAgendamentosByDataHora(dthora);
+            if (diaHora.isEmpty()) {
+                return ResponseEntity.status(204).body(null);
+            }
+            return ResponseEntity.ok(diaHora); // Retorna a lista completa
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Erro ao buscar material: " + e.getMessage());
+        }
+    }
+*/
+
+
+
+
+    @PostMapping
+    public ResponseEntity<?> postAgendamento( @RequestBody  Agendamento agendamento){
+        try{
+            Agendamento novaAgenda = service.postAgendamento(agendamento);
+            return ResponseEntity.status(201).body(novaAgenda);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(409).body("Erro ao salvar agendamento" + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Erro ao salvar agendamento" + e.getMessage());
+        }
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putAgendamento(@PathVariable Long id, @RequestBody @Valid Agendamento agendamento){
+        try{
+            Agendamento novaAgenda = service.putAgendamentoById(id, agendamento);
+            return ResponseEntity.status(204).body(null);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(404).body("Erro ao atualizar a agenda" + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Erro ao atualizar a agenda" + e.getMessage());
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAgendamento(@PathVariable Long id){
+        try{
+             service.deleteAgendamentoById(id);
+            return ResponseEntity.status(204).body(null);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(404).body("Erro excluir agenda" + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body("Erro exluir agenda" + e.getMessage());
+        }
+    }
+
+
 }
+
+
--- Back-end/src/main/java/hub/orcana/service/AgendamentoService.java
+++ Back-end/src/main/java/hub/orcana/service/AgendamentoService.java
@@ -1,13 +1,91 @@
 package hub.orcana.service;
 
+import hub.orcana.tables.Agendamento;
 import hub.orcana.tables.repository.AgendamentoRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Service
 public class AgendamentoService {
     private final AgendamentoRepository repository;
 
+
     public AgendamentoService(AgendamentoRepository repository) {
         this.repository = repository;
     }
+
+
+    // Lista todos os agendamentos existentes
+    public List<Agendamento> getAgendamentos() {
+        return repository.findAll();
+    }
+
+    //Busca agendamento pelo id
+    public Agendamento getAgendamentoPorId(Long id) {
+        return repository.findById(id).orElseThrow(() -> new IllegalArgumentException("Agendamento não encontrado."));
+    }
+
+
+    // Busca agendamentos por status
+    public List<Agendamento> getAgendamentosByStatus( String status) {
+        var agendamentosStatus = repository.findAll()
+                .stream()
+                .filter(atual -> atual.getStatus().name().equalsIgnoreCase(status))
+                .toList();
+
+        return agendamentosStatus;
+    }
+
+    // Busca agendamentos por data hora
+  /*  public List<Agendamento> getAgendamentosByDataHora( String dataHora) {
+        LocalDateTime data = LocalDateTime.parse(dataHora);
+        return repository.findAll()
+                .stream()
+                .filter(atual -> atual.getDataHora().equals(data))
+                .toList();
+    }
+*/
+
+    // Cadastra um novo agendamento
+    public Agendamento postAgendamento(@RequestBody Agendamento agendamento) {
+        if (agendamento.getId() != null) {
+            if (repository.existsById(agendamento.getId())) {
+                throw new IllegalArgumentException("O ID do agendamento já existe.");
+            } else {
+                agendamento.setId(null); // Garante que será criado novo
+            }
+        }
+
+        return repository.save(agendamento);
+    }
+
+    // Atualiza um agendamento existente pelo ID
+    public Agendamento putAgendamentoById(@PathVariable Long id, @RequestBody Agendamento agendamento) {
+        if (!repository.existsById(id)) {
+            throw new IllegalArgumentException("Agendamento não encontrado.");
+        }
+
+        agendamento.setId(id);
+        return repository.save(agendamento);
+    }
+
+    // Exclui um agendamento existente pelo ID
+    public void deleteAgendamentoById(@PathVariable Long id) {
+        if (!repository.existsById(id)) {
+            throw new IllegalArgumentException("Agendamento não encontrado.");
+        }
+
+        repository.deleteById(id);
+
+        if (repository.existsById(id)) {
+            throw new IllegalArgumentException("Erro ao excluir agendamento.");
+        }
+    }
 }
+

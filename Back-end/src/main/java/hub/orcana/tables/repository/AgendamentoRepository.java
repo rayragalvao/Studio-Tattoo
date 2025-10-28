78c6a151eb7a86b889faa2841fca1d64d0623306
vivids12 <viviane.santos@sptech.school>
2025-10-28 12:23:13 -0300

    CRUD de relacionamento da Rayra funcionando

--- Back-end/src/main/java/hub/orcana/tables/repository/AgendamentoRepository.java
+++ Back-end/src/main/java/hub/orcana/tables/repository/AgendamentoRepository.java
@@ -2,6 +2,10 @@ package hub.orcana.tables.repository;
 
 import hub.orcana.tables.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    // busca todos os agendamentos de um usuário específico
+    List<Agendamento> findByUsuarioId(Long usuarioId);
 }

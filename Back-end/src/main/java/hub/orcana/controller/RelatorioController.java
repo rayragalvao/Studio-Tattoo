2cbcc395b4f14850b582aef8ffbbf6f60f84a3c9
vivisds12 <viviane.santos@sptech.school>
2025-11-13 07:34:55 -0300

    deletei a pasta de front da develop

--- Back-end/src/main/java/hub/orcana/controller/RelatorioController.java
+++
@@ -1,9 +0,0 @@
-package hub.orcana.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/relatorio")
-public class RelatorioController {
-}
